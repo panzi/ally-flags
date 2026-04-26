a0a655588a1a03edea3715ea4b18428eb57e667b
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2026-04-27 01:40:30 +0200

    fix style on mobile devices

--- mkhtml.py
+++ mkhtml.py
@@ -67,6 +67,7 @@ f"""\
 <html lang="en">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Ally Flag Variations</title>
 <link rel="stylesheet" href="style.css">
 </head>
